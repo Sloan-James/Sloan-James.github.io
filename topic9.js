1e97e33b6a187c417da733f1f5b370c50537f45e
Sloan-James <staresatcomps@gmail.com>
2018-02-04 18:43:38 -0700

    potential fix

--- topic9.js
+++ topic9.js
@@ -1,10 +1,12 @@
 function displayDate() {
-  document.getElementById("test").innerHTML = Date().toDateString();
+  d = Date();
+  document.getElementById("test").innerHTML = d.toDateString();
 }
 
 function draw(e){
   var c = document.getElementById("drawCanvas");
   var ctx = c.getContext("2d");
+  ctx.fillStyle = "black";
   ctx.beginPath();
   ctx.moveTo(e.clientX,e.clientY);
   ctx.lineTo(e.clientX,e.clientY);
