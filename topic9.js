6b556b1876f714f78c8526013da066a02d8a01d7
Sloan-James <staresatcomps@gmail.com>
2018-02-04 18:24:51 -0700

    onload and onmousedown

---
+++ topic9.js
@@ -0,0 +1,12 @@
+function displayDate(el) {
+  el.innerHTML = Date().toDateString();
+}
+
+function draw(e){
+  var c = document.getElementById("drawCanvas");
+  var ctx = c.getContext("2d");
+  ctx.beginPath();
+  ctx.moveTo(e.clientX,e.clientY);
+  ctx.lineTo(e.clientX,e.clientY);
+  ctx.stroke();
+}
