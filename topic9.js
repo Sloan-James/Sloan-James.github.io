b658b5713cf2db22da0f11ef16ced017a9803bd9
Sloan-James <staresatcomps@gmail.com>
2018-02-05 20:45:23 -0700

    stuff

--- topic9.js
+++ topic9.js
@@ -42,7 +42,7 @@ if(!touchAvailable){
 /*Animation*/
   var anim = document.getElementById("anim");
   anim.addEventListener("click",ToggleAnimation, false);
-  anim.addEventListener("ontouchstart",ToggleAnimation,false);
+  anim.addEventListener("ontouchend",ToggleAnimation,false);
   PrefixedEvent(anim, "AnimationStart", AnimationListener);
   PrefixedEvent(anim, "AnimationIteration", AnimationListener);
   PrefixedEvent(anim, "AnimationEnd", AnimationListener);
