74340fe8571fb07613e53d631afc98a6449f1fea
Sloan-James <staresatcomps@gmail.com>
2018-02-17 23:39:10 -0700

    Topic 1 ready

--- topic1.js
+++ topic1.js
@@ -96,27 +96,32 @@ function validateDate(date) {
     var dd = parseInt(values[1]);
     var yyyy = parseInt(values[2]);
 
-    var ListofDays = [31,28,31,30,31,30,31,31,30,31,30,31];
-    if ((mm==1 || mm>2) && !(mm > 12 || mm < 1)) {
-      if(dd>ListofDays[mm-1]) {
-        alert("Invalid date format");
+    if(!(mm > 12 || mm < 1)) {
+      var ListofDays = [31,28,31,30,31,30,31,31,30,31,30,31];
+      if ((mm==1 || mm>2)) {
+        if(dd>ListofDays[mm-1]) {
+          alert("Invalid date format");
         return false;
+        }
+      } else {
+        var lyear = false;
+        if ( (!(yyyy % 4) && yyyy % 100) || !(yyyy %400)) {
+          lyear = true;
+        }
+        if ((lyear == false) && (dd >= 29)) {
+          alert("Invalid date");
+          return false;
+        }
+        if ((lyear == true) && (dd > 29)) {
+          alert("Invalid date");
+          return false;
+        }
       }
+      return true;
     } else {
-      var lyear = false;
-      if ( (!(yyyy % 4) && yyyy % 100) || !(yyyy %400)) {
-        lyear = true;
-      }
-      if ((lyear == false) && (dd >= 29)) {
-        alert("Invalid date format");
-        return false;
-      }
-      if ((lyear == true) && (dd > 29)) {
-        alert("Invalid date format");
-        return false;
-      }
+      alert("Invalid date");
+      return false;
     }
-    return true;
   } else {
     alert("Invalid date format");
     return false;
